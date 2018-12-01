b5140f20ac07b23c30a720ae953b91b042e10991
unknown <andre11gamer@gmail.com>
2018-12-01 23:13:14 +0000

    main window html menu

--- pCreate/main.js
+++ pCreate/main.js
@@ -1,6 +1,54 @@
 const electron = require('electron');
-const { app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
-function createWindow() {
-    
+var startedWin;
+
+function createStartWindow() {
+    let template = [];
+    if (process.platform == 'darwin') {
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                {role: 'quit'}
+            ]
+        });
+    } else {
+        template.unshift({
+            label: 'File',
+            submenu: [
+                {
+                    label: 'quit',
+                    click() {
+                        app.quit();
+                    }
+                }
+            ]
+        });
+    }
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    startedWin = new BrowserWindow({width: 800, height: 600, resizable: false});
+
+    startedWin.webContents.openDevTools();
+
+    startedWin.loadFile('windows/startwin.html');
+
+    startedWin.on('closed', () => {
+        startedWin = null;
+    });
 }
+
+app.on('ready', createStartWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createStartWindow()
+    }
+})
