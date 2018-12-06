babd8dd21d6953ced2e90a26ad592538444e8960
unknown <andre11gamer@gmail.com>
2018-12-06 21:25:28 +0000

    startwin finised main win started

--- pCreate/main.js
+++ pCreate/main.js
@@ -1,7 +1,13 @@
 const electron = require('electron');
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+const fs = require('fs');
+const path = require('path');
 
+/***@type BrowserWindow*/
 var startedWin;
+var mainWindow;
+var last;
+var workingDir;
 
 function createStartWindow() {
     let template = [];
@@ -37,11 +43,47 @@ function createStartWindow() {
     });
 }
 
-app.on('ready', createStartWindow);
+function createMainWindow() {
+    let template = [];
+    if (process.platform == 'darwin') {
+        template.unshift({
+            label: app.getName(),
+            submenu: [
+                {role: 'quit'}
+            ]
+        });
+    } else {
+        template.unshift({
+            label: 'File',
+            submenu: [
+                {
+                    label: 'quit',
+                    click() {
+                        app.quit();
+                    }
+                }
+            ]
+        });
+    }
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.loadFile('windows/mainwin.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createStartWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-      app.quit()
+      app.quit();
     }
 })
 
@@ -52,3 +94,77 @@ app.on('activate', () => {
       createStartWindow()
     }
 })
+
+ipcMain.on('load', (event, msg) => {
+    if (msg == '||:last') {
+        getLastProject();
+        if (last == null) {
+            startedWin.webContents.send('errorM', 'No last project opened!');
+            return;
+        }
+        workingDir = last;
+    } else if (msg == '||:open') {
+
+    } else {
+        let dir = require('os').homedir();
+        dir = path.join(dir, 'Documents');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        dir = path.join(dir, 'AP');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        dir = path.join(dir, msg);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        dir = path.join(dir, msg + '.JSON');
+        let json = {
+            "name": msg,
+            "slidePre": [],
+            "slideScren": []
+        };
+        if (!fs.existsSync(dir)) {
+            fs.writeFileSync(dir, JSON.stringify(json));
+        }
+        let data = fs.readFileSync(dir);
+        json = JSON.parse(data);
+        workingDir = dir;
+
+    }
+});
+
+function getLastProject() {
+    let dir = require('os').homedir();
+    dir = path.join(dir, 'Documents');
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    dir = path.join(dir, 'AP');
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    dir = path.join(dir, 'config');
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    dir = path.join(dir, 'config.txt');
+    if (!fs.existsSync(dir)) {
+        let toJSON = {
+            "last": "||:none"
+        };
+        fs.writeFileSync(dir, JSON.stringify(toJSON));
+        last = null;
+        return;
+    }
+    let data = fs.readFileSync(dir);
+    let fromJSON = JSON.parse(data);
+    if (fromJSON.last == '||:none') {
+        last = null;
+        return;
+    } else {
+        last = fromJSON.last;
+        return;
+    }
+}
---
+++ pCreate/windows/startwin.js
@@ -0,0 +1,34 @@
+const {ipcRenderer} = require('electron'); 
+
+function last() {
+    ipcRenderer.send('load', "||:last");
+}
+
+function newP() {
+    /*** 
+     * @type String
+     */
+    var text = document.getElementById('newName').value;
+    if (text.indexOf('\"') != -1 || text.indexOf('\'') != -1 || text.indexOf(':') != -1 || text.indexOf(':') != -1 || text == '' || text.indexOf(' ') != -1) {
+        document.getElementById('errordiv').classList.remove('disabled');
+        document.getElementById('divmessage').classList.remove('disabled');
+        document.getElementById('perror').innerHTML = 'Invalid character in the name!';
+        return;
+    }
+    ipcRenderer('load', text);
+}   
+
+function open() {
+    ipcRenderer.send('load', "||:open");
+}
+
+function closeerror() {
+    document.getElementById('errordiv').classList.add('disabled');
+    document.getElementById('divmessage').classList.add('disabled');
+}
+
+ipcRenderer.on('errorM', (event1, msg) => {
+    document.getElementById('errordiv').classList.remove('disabled');
+    document.getElementById('divmessage').classList.remove('disabled');
+    document.getElementById('perror').innerHTML = msg;
+});
